925724e0f87c6ab8926fb32492713e8b92e95fb9
Theo Kandi Bikoko Pineda <25634122r@iespoblenou.org>
2024-12-01 04:18:30 +0100

    Add modulus method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,26 +1,30 @@
 public class Calculator {
-        public int add(int a, int b) {
-            return a + b;
-        }
-        public int subtract(int a, int b) {
-            return a - b;
-        }
-        public int multiply(int a, int b) {
-            return a * b;
-        }
-        public int divide(int a, int b) {
-            try{
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b) {
+        try{
             return a / b;
-            }catch(ArithmeticException e){
-                System.out.println("Dividir entre 0 es impossible. Per tant, retornem el resultat 0");
-                return 0;
-            }
-        }
-        public static void main(String[] args) {
-            Calculator calculator = new Calculator();
-            System.out.println("Addition: " + calculator.add(10, 5));
-            System.out.println("Subtraction: " + calculator.subtract(10, 5));
-            System.out.println("Multiplication: " + calculator.multiply(10, 5));
-            System.out.println("Division: " + calculator.divide(10, 0));
+        }catch(ArithmeticException e){
+            System.out.println("Dividir entre 0 es impossible. Per tant, retornem el resultat 0");
+            return 0;
         }
     }
+    public int modulus(int a, int b) {
+        return a % b;
+}
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Addition: " + calculator.add(10, 5));
+        System.out.println("Subtraction: " + calculator.subtract(10, 5));
+        System.out.println("Multiplication: " + calculator.multiply(10, 5));
+        System.out.println("Division: " + calculator.divide(10, 0));
+        System.out.println("Modulus: " + calculator.modulus(10, 7));
+    }
+}
